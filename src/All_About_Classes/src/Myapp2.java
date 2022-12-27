cfbb2cd248acd9739038b9eba9a454415cbcab48
Malhar <elixir0malhar@gmail.com>
2022-12-28 01:20:19 +0530

    added Myapp2

---
+++ src/All_About_Classes/src/Myapp2.java
@@ -0,0 +1,7 @@
+public class Myapp2 {
+    public static void main(String args[]){
+        VisitorForm2 Jay = new VisitorForm2();
+        Jay.setAge(20);
+        System.out.println(Jay.getAge());
+    }
+}
